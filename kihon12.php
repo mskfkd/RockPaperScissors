23cf35b6e39fa76db9eaa301c1f9c851f59e73db
mskfkd <fm6096@yahoo.co.jp>
2020-03-02 22:19:43 +0900

    とりあえず動くようになった

--- kihon12.php
+++ kihon12.php
@@ -23,17 +23,16 @@ function checkHand($inputHand){
 function masterHand() {
 //PC側の手をランダムに選択
 	$selectHand = array("グー" , "チョキ" , "パー");
-	$ansHand = array_rand($selectHand);
-	return $ansHand;
+	$ans = array_rand($selectHand);
+	return $selectHand[$ans];
 }
 
 //勝敗判定
-function judge($inputHand) {
-	$key = masterHand();
+function judgeStone($inputHand) {
 
-	if($key === "グー") {
 		switch ($inputHand) {
-			case ($key === $inputHand):
+
+			case "グー":
 				$result = "あいこ";
 				break;
 			
@@ -48,10 +47,13 @@ function judge($inputHand) {
 
 		return $result;
 
-	}elseif ($key === "チョキ") {
+}
+
+function judgeScissor($inputHand) {
+
 		switch ($inputHand) {
 			
-		case ($key === $inputHand):
+			case "チョキ":
 				$result = "あいこ";
 				break;
 			
@@ -66,48 +68,87 @@ function judge($inputHand) {
 
 		return $result;
 
-	}elseif($key === "パー") {
+}
+
+function judgePaper($inputHand) {
+
 		switch ($inputHand) {
 			
-		case ($key === $inputHand):
+			case "パー":
 				$result = "あいこ";
 				break;
 			
 			case "チョキ":
-				$result = "負け";
+				$result = "勝ち";
 				break;
 
-			case "パー":
-				$result = "勝ち";
+			case "グー":
+				$result = "負け";
 				break;
 		}
 
 		return $result;
 
-	}else{
-		echo "エラーです";
-		return;
-	}
 }
 
 //勝負処理
-function  RockPaperScissors($inputHand) {
+function  RockPaperScissors() {
 	$inputHand = trim(fgets(STDIN));
+	$key = masterHand();
 
-	if(checkHand($inputHand) === true) {
-		$judge = judge($inputHand);
-		return $judge;
-	}else{
-		return RockPaperScissors($inputHand);
+	echo "あなたは" . $inputHand . "を出すのですね?";
+
+	if($key === "グー") {
+
+			echo "PCは" . $key . "を出しました。" . "\n";
+			$checkans = checkHand($inputHand);
+
+		if($checkans === true) {
+
+			$judgeans = judgeStone($inputHand);
+			return $judgeans;
+
+		}else{
+
+			echo "エラーです。";
+			
+		}
+	}elseif ($key === "チョキ") {
+
+			echo "PCは" . $key . "を出しました。" . "\n";
+			$checkans = checkHand($inputHand);
+
+		if ($checkans === true) {
+
+			$judgeans = judgeScissor($inputHand);
+			return $judgeans;
+
+		}else{
+
+			echo "エラーです。";
+
+		}
+	}elseif ($key === "パー") {
+
+			echo "PCは" . $key . "を出しました。" . "\n";
+			$checkans = checkHand($inputHand);
+
+		if ($checkans === true) {
+
+			$judgeans = judgePaper($inputHand);
+			return $judgeans;
+
+		}else{
+
+			echo "エラーです。";
+
+		}
 	}
 }
 
 //見える側
-echo "じゃんけんをしましょう。あなたの手を入力してください。";
-$player = RockPaperScissors(trim(fgets(STDIN)));
-echo "あなたは" . $player . "を出すのですね?";
-$gamemaster = masterHand($gamemaster);
-echo "PCは" . $gamemaster . "を出しました。";
-echo "あなたは" . $player . "です。";
+echo "じゃんけんをしましょう。あなたの手を入力してください。" . "\n";
+$player = RockPaperScissors();
+echo "あなたは" . $player . "です。" . "\n";
 
 ?>
